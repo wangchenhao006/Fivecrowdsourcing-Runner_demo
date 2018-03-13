fb629c8e7611f374b7c98d758835e53dcae9f190
wch <wangchenhao006@126.com>
2018-03-13 14:12:44 +0800

    listview

--- android/app/src/main/java/com/example/wun/testservlet/Activity/testActivity.java
+++ android/app/src/main/java/com/example/wun/testservlet/Activity/testActivity.java
@@ -3,113 +3,80 @@ package com.example.wun.testservlet.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-
-import com.example.wun.testservlet.R;
-import  com.example.wun.testservlet.Adapter.OrderAdapter;
-import  com.example.wun.testservlet.Bean.Order.*;
+import android.content.Context;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import com.example.wun.testservlet.Bean.OrderBean;
+import com.example.wun.testservlet.Adapter.OrderAdapter;
+import  com.example.wun.testservlet.R;
+
 import java.util.List;
 
+/**
+ * MerchandiseListActivity
+ * <p>
+ * author: wch<br>
+ * time:   2017803/11 18:03 <br>
+ * GitHub: https://github.com/wangchenhao006
+ * CSDN:   http://blog.csdn.net/wangchenhao006
+ * 实现订单信息的显示。
+ * </p>
+ */
 public class testActivity extends AppCompatActivity {
-
-    private RecyclerView rlvAllOrder;
-    private OrderAdapter mAllOrderAdapter;
-    private List<Object> mAllOrderList = new ArrayList<>();
+    //OrderBean OrderBean;
+   // private List<OrderBean> mData = null;
+    private Context mContext;
+    private OrderAdapter mAdapter = null;
+    private ListView list_view;
+    private List<OrderBean> mOrder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testlayout);
+        list_view = (ListView) findViewById(R.id.list_test);
+        mContext = testActivity.this;
+        bindViews();
+        List<OrderBean> mData = new ArrayList<>();
+        mData = new ArrayList<OrderBean>();
+        mOrder = new LinkedList<OrderBean>();
+        /*mData.add(new OrderBean("001", "订单1", R.mipmap.ic_icon_qitao));
+        mData.add(new OrderBean("002", "订单2", R.mipmap.ic_icon_qitao));
+        mData.add(new OrderBean("003", "订单3", R.mipmap.ic_icon_qitao));
+        mData.add(new OrderBean("004", "订单4", R.mipmap.ic_icon_qitao));
+        mData.add(new OrderBean("005", "订单5", R.mipmap.ic_icon_qitao));*/
+        /*for(int i=0;i<10;i++){
+            OrderBean ob=new OrderBean("001", "订单1", R.drawable.ic_launcher);
+            ob.setOrderId("10"+i);
+            ob.setTitle("name"+i);
+            ob.setIcon(R.drawable.ic_launcher_round);
+            mData.add(ob);
+        }*/
+        //System.out.println(mData.toString());
+        Log.e("wch", "onCreate: orderbean ");
+        mAdapter = new OrderAdapter( mData, testActivity.this);
 
-        rlvAllOrder = (RecyclerView) findViewById(R.id.rlv_allorder);
 
-       // LinearLayoutManager layoutManager1=new LinearLayoutManager(testActivity.this,LinearLayoutManager.VERTICAL,true);
-        rlvAllOrder.setLayoutManager(new LinearLayoutManager(this));
-        //rlvAllOrder.setLayoutManager(layoutManager1);
-
-        mAllOrderAdapter = new OrderAdapter(this, mAllOrderList);
-        rlvAllOrder.setAdapter(mAllOrderAdapter);
-        initData();
-        mAllOrderAdapter.notifyDataSetChanged();
-
+        //list_view = (ListView) findViewById(R.id.list_test);
+        list_view.setAdapter(mAdapter);
     }
 
-    private void initData(){
-        AllOrderBean allOrderBean = new AllOrderBean();
-        OrderSummary orderSummary1 = new OrderSummary();
-        OrderSummary orderSummary2 = new OrderSummary();
-        OrderGoodsItem orderGoodsItem1 = new OrderGoodsItem();
-        OrderGoodsItem orderGoodsItem2 = new OrderGoodsItem();
-        OrderGoodsItem orderGoodsItem3 = new OrderGoodsItem();
-        OrderGoodsItem orderGoodsItem4 = new OrderGoodsItem();
-        GoodsOrderInfo goodsOrderInfo1 = new GoodsOrderInfo();
-        GoodsOrderInfo goodsOrderInfo2 = new GoodsOrderInfo();
-        GoodsOrderInfo goodsOrderInfo3 = new GoodsOrderInfo();
-        GoodsOrderInfo goodsOrderInfo4 = new GoodsOrderInfo();
+    /*@Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(mContext,"你点击了第" + position + "项",Toast.LENGTH_SHORT).show();
+    }*/
 
-        orderSummary1.setId(1);
-        orderSummary1.setTotalPrice(10);
-        orderSummary1.setStatus("0");
-        orderSummary1.setOrderCode("2016111301");
-        goodsOrderInfo1.setOrderCode("2016111301");
-        goodsOrderInfo1.setStatus("0");
-        goodsOrderInfo1.setShopName("狼牙");
-        orderGoodsItem1.setTotalPrice(5);
-        orderGoodsItem1.setProductName("狼牙狼蛛键盘");
-        orderGoodsItem1.setCount(2);
-        orderGoodsItem1.setProductPic("http://img2.3lian.com/2014/c7/25/d/40.jpg");
-        orderGoodsItem1.setOrder(goodsOrderInfo1);
+    private void bindViews(){
+        list_view = (ListView) findViewById(R.id.list_test);
+        TextView txt_empty = (TextView) findViewById(R.id.txt_empty);
 
-        goodsOrderInfo2.setOrderCode("2016111302");
-        goodsOrderInfo2.setStatus("0");
-        goodsOrderInfo2.setShopName("达尔优");
-        orderGoodsItem2.setTotalPrice(5);
-        orderGoodsItem2.setProductName("牧马人鼠标");
-        orderGoodsItem2.setCount(2);
-        orderGoodsItem2.setProductPic("http://img2.3lian.com/2014/c7/25/d/41.jpg");
-        orderGoodsItem2.setOrder(goodsOrderInfo2);
-
-        goodsOrderInfo4.setOrderCode("2016111302");
-        goodsOrderInfo4.setStatus("0");
-        goodsOrderInfo4.setShopName("达尔优");
-        orderGoodsItem4.setTotalPrice(5);
-        orderGoodsItem4.setProductName("牧马人键盘");
-        orderGoodsItem4.setCount(2);
-        orderGoodsItem4.setProductPic("http://img2.3lian.com/2014/c7/25/d/41.jpg");
-        orderGoodsItem4.setOrder(goodsOrderInfo4);
-
-        List<OrderGoodsItem> orderGoodsItemList = new ArrayList<>();
-        orderGoodsItemList.add(orderGoodsItem1);
-        orderGoodsItemList.add(orderGoodsItem4);
-        orderGoodsItemList.add(orderGoodsItem2);
-        orderSummary1.setOrderDetailList(orderGoodsItemList);
-
-        orderSummary2.setId(2);
-        orderSummary2.setTotalPrice(10);
-        orderSummary2.setStatus("1");
-        orderSummary2.setOrderCode("2016111303");
-        goodsOrderInfo3.setOrderCode("2016111303");
-        goodsOrderInfo3.setStatus("0");
-        goodsOrderInfo3.setShopName("酷睿");
-        orderGoodsItem3.setTotalPrice(5);
-        orderGoodsItem3.setProductName("酷睿处理器");
-        orderGoodsItem3.setCount(2);
-        orderGoodsItem3.setProductPic("http://imgsrc.baidu.com/forum/pic/item/b64543a98226cffc8872e00cb9014a90f603ea30.jpg");
-        orderGoodsItem3.setOrder(goodsOrderInfo3);
-
-        List<OrderGoodsItem> orderGoodsItemList1 = new ArrayList<>();
-        orderGoodsItemList1.add(orderGoodsItem3);
-        orderSummary2.setOrderDetailList(orderGoodsItemList1);
-
-        List<OrderSummary> orderSummaries = new ArrayList<>();
-        orderSummaries.add(orderSummary1);
-        orderSummaries.add(orderSummary2);
-        allOrderBean.setResultList(orderSummaries);
-
-        mAllOrderList.addAll(OrderDataHelper.getDataAfterHandle(allOrderBean.getResultList()));
+        txt_empty.setText("暂无数据~");
+        list_view.setEmptyView(txt_empty);
     }
+
 }
--- android/app/src/main/java/com/example/wun/testservlet/Adapter/OrderAdapter.java
+++ android/app/src/main/java/com/example/wun/testservlet/Adapter/OrderAdapter.java
@@ -1,206 +1,86 @@
 package com.example.wun.testservlet.Adapter;
-
-/**
- * Created by WUN on 2018/3/8.
- */
-
+import com.example.wun.testservlet.Bean.OrderBean;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 
-import com.bumptech.glide.Glide;
-
-import com.example.wun.testservlet.R;
-
-import  com.example.wun.testservlet.Bean.Order.*;
-
-
+import java.util.LinkedList;
 import java.util.List;
-
+import android.util.Log;
+import com.example.wun.testservlet.R;
 /**
- * Created by Administrator on 2016/11/9.
+ * Created by WUN on 2018/3/11.
  */
 
-public class OrderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class OrderAdapter extends BaseAdapter {
+    //OrderBean OrderBean;
+    private List<OrderBean> mData;
+    //private Context mContext;
+    private LayoutInflater inflater;
 
-    private Context context;
-    private List<Object> data;
-    private View headerView;
-    private int ITEM_HEADER = 1,ITEM_CONTENT = 2,ITEM_FOOTER = 3;
+    public OrderAdapter(List<OrderBean> mData, Context mContext) {
 
-    public OrderAdapter(Context context, List<Object> data){
-        this.context = context;
-        this.data = data;
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view;
-        if(viewType == ITEM_HEADER) {
-            view = LayoutInflater.from(context).inflate(R.layout.item_allorder_header, parent, false);
-
-            return new OrderAdapter.MyViewHolderHeader(view);
-        }else if(viewType == ITEM_CONTENT){
-            view = LayoutInflater.from(context).inflate(R.layout.item_allorder_content, parent, false);
-            return new OrderAdapter.MyViewHolderContent(view);
-        }else if(viewType == ITEM_FOOTER){
-            view = LayoutInflater.from(context).inflate(R.layout.item_allorder_footer, parent, false);
-            return new OrderAdapter.MyViewHolderFooter(view);
-        }
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-        if(holder instanceof MyViewHolderHeader){
-            GoodsOrderInfo datas = (GoodsOrderInfo)data.get(position);
-            ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemid.setText("订单编号：" + datas.getOrderCode());
-            ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemShopName.setText(datas.getShopName());
-            if(datas.getStatus().equals("0")){
-                ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemState.setText("待付款");
-            }else if(datas.getStatus().equals("1")){
-                ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemState.setText("待发货");
-            }else if(datas.getStatus().equals("2")){
-                ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemState.setText("待收货");
-            }else if(datas.getStatus().equals("3")){
-                ((OrderAdapter.MyViewHolderHeader) holder).tvAllOrderItemState.setText("交易完成");
-            }
-        }else if(holder instanceof MyViewHolderContent) {
-            OrderGoodsItem datas = (OrderGoodsItem)data.get(position);
-            Glide.with(context).load(datas.getProductPic())
-                    .into(((OrderAdapter.MyViewHolderContent) holder).ivAllOrderItemPic);
-            ((OrderAdapter.MyViewHolderContent) holder).tvAllOrderItemTitle.setText(datas.getProductName());
-            ((OrderAdapter.MyViewHolderContent) holder).tvAllOrderItemNum.setText("共" + datas.getCount() + "件");
-            ((OrderAdapter.MyViewHolderContent) holder).tvAllOrderItemPrice.setText("¥" + datas.getTotalPrice());
-
-            final int pos = datas.getOrderid();
-
-            ((MyViewHolderContent) holder).llAllOrderItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    /*Intent intent = new Intent(context, OrderDetailActivity.class);
-                    Bundle bundle = new Bundle();
-                    //类型是确认订单
-                    bundle.putString("mType", "orderdetail");
-                    bundle.putInt("mOrderId",pos);
-                    intent.putExtras(bundle);
-                    context.startActivity(intent);*/
-                }
-            });
-
-        }else if(holder instanceof MyViewHolderFooter) {
-            OrderPayInfo datas = (OrderPayInfo)data.get(position);
-            ((OrderAdapter.MyViewHolderFooter) holder).tvAllOrderTotal.setText(datas.getTotalAmount() + "");
-            final int pos = datas.getId();
-            if(datas.getStatus().equals("0")){
-                ((OrderAdapter.MyViewHolderFooter) holder).tvAllOrderSubmit.setText("付款");
-            }else {
-                ((OrderAdapter.MyViewHolderFooter) holder).tvAllOrderSubmit.setText("商品详情");
-            }
-
-            ((MyViewHolderFooter) holder).tvAllOrderSubmit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    /*Intent intent = new Intent(context, OrderDetailActivity.class);
-                    Bundle bundle = new Bundle();
-                    //类型是确认订单
-                    bundle.putString("mType", "orderdetail");
-                    bundle.putInt("mOrderId",pos);
-                    intent.putExtras(bundle);
-                    context.startActivity(intent);*/
-                }
-            });
-        }
+        this.mData = mData;
+       // this.mContext = mContext;
+        this.inflater=LayoutInflater.from(mContext);
 
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if(data.get(position) instanceof GoodsOrderInfo) {
-            return ITEM_HEADER;
-        }else if(data.get(position) instanceof OrderGoodsItem){
-            return ITEM_CONTENT;
-        }else if(data.get(position) instanceof OrderPayInfo){
-            return ITEM_FOOTER;
-        }
-        return ITEM_CONTENT;
+    public int getCount() {
+        return mData==null?0:mData.size();
     }
 
     @Override
-    public int getItemCount() {
-        int count = (data == null ? 0 : data.size());
-        if(headerView != null){
-            count++;
+    public OrderBean getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.v("MyListViewBase", "getView " + position + " " + convertView);
+
+        ViewHolder holder = null;
+
+        if(convertView == null) {
+            convertView = inflater.inflate(R.layout.list_order, parent,false);
+            holder = new ViewHolder();
+            holder.img_icon = (ImageView) convertView.findViewById(R.id.imgicon);
+            holder.orderid = (TextView) convertView.findViewById(R.id.orderid);
+            holder.ordertitle = (TextView) convertView.findViewById(R.id.ordertitle);
+            convertView.setTag(holder);   //将Holder存储到convertView中
         }
-        return count;
-    }
-
-
-    class MyViewHolderContent extends RecyclerView.ViewHolder
-    {
-        private ImageView ivAllOrderItemPic;
-        private TextView tvAllOrderItemTitle;
-        private TextView tvAllOrderItemPrice;
-        private LinearLayout llAllOrderItem;
-        private TextView tvAllOrderItemNum;
-
-        public MyViewHolderContent(View view)
-        {
-            super(view);
-            llAllOrderItem = (LinearLayout) view.findViewById(R.id.ll_item_allorder);
-            ivAllOrderItemPic = (ImageView) view.findViewById(R.id.iv_item_allorder_pic);
-            tvAllOrderItemTitle = (TextView) view.findViewById(R.id.tv_item_allorder_title);
-            tvAllOrderItemPrice = (TextView) view.findViewById(R.id.tv_item_allorder_item_price);
-            tvAllOrderItemNum = (TextView) view.findViewById(R.id.tv_item_allorder_item_num);
+        else{
+            holder = (ViewHolder) convertView.getTag();
         }
+
+        //OrderBean orderBean=getItem(position);
+
+        /*ImageView img_icon = (ImageView) convertView.findViewById(R.id.imgicon);
+        TextView orderid = (TextView) convertView.findViewById(R.id.orderid);
+        TextView ordertitle = (TextView) convertView.findViewById(R.id.ordertitle);*/
+        //img_icon.setImageResource(orderBean.getIcon());
+        holder.img_icon.setImageResource(mData.get(position).getIcon());
+        Log.v("setImageResource", "Icon: " + mData.get(position).getIcon());
+
+        holder.orderid.setText(mData.get(position).getOrderId());
+       holder.ordertitle.setText(mData.get(position).getTitle());
+        return convertView;
     }
 
-    class MyViewHolderHeader extends RecyclerView.ViewHolder
-    {
-        private LinearLayout llAllOrderItem;
-        private TextView tvAllOrderItemState,tvAllOrderItemid,tvAllOrderItemShopName;
-
-        public MyViewHolderHeader(View view)
-        {
-            super(view);
-            llAllOrderItem = (LinearLayout) view.findViewById(R.id.ll_item_allorder_header);
-            tvAllOrderItemid = (TextView) view.findViewById(R.id.tv_item_allorder_orderid);
-            tvAllOrderItemShopName = (TextView) view.findViewById(R.id.tv_item_allorder_shopname);
-            tvAllOrderItemState = (TextView) view.findViewById(R.id.tv_item_allorder_state);
-        }
-    }
-
-    class MyViewHolderFooter extends RecyclerView.ViewHolder
-    {
-        private TextView tvAllOrderTotal,tvAllOrderSubmit;
-
-        public MyViewHolderFooter(View view)
-        {
-            super(view);
-            tvAllOrderTotal = (TextView) view.findViewById(R.id.tv_item_allorder_total);
-            tvAllOrderSubmit = (TextView) view.findViewById(R.id.tv_item_allorder_submit);
-        }
-    }
-
-
-    public View getHeaderView(){
-        return headerView;
-    }
-
-    private OrderAdapter.OnItemClickListener mOnItemClickListener;
-    public interface OnItemClickListener{
-        void onItemClick(View view, int position);
-    }
-
-    public void setOnItemClickListener(OrderAdapter.OnItemClickListener mOnItemClickListener){
-        this.mOnItemClickListener = mOnItemClickListener;
-    }
+static class ViewHolder{
+    ImageView img_icon;
+    TextView orderid;
+    TextView ordertitle;
+}
 }
